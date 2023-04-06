45614527e4a7c4442b1167287ad427b7db053846
glaucio.junior <glaucioripol@gmail.com>
2023-04-06 17:29:44 -0300

    feat: adding icon to resource

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+
 import { UsersList, PostList, PostEdit, PostCreate } from "./modules";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
@@ -12,8 +15,14 @@ const App = () => (
       list={PostList}
       edit={PostEdit}
       create={PostCreate}
+      icon={PostIcon}
     />
-    <Resource name="users" list={UsersList} recordRepresentation="name" />
+    <Resource
+      name="users"
+      list={UsersList}
+      recordRepresentation="name"
+      icon={UserIcon}
+    />
   </Admin>
 );
 
